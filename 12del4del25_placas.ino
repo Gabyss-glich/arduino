8208f449dd44412ebb755fa36bd083e787ef983e
Glich de Gabyss <guaduleto2011@gmail.com>
2025-04-30 20:40:40 -0300

    Primera subida

---
+++ 12del4del25_placas.ino
@@ -0,0 +1,34 @@
+#define buzzer_pin 45
+#define C 262
+#define D 294
+#define E 330
+#define F 349
+#define G 392
+#define A 440
+#define B 494
+
+#define PLAY_TONE(freq, dur) tone(buzzer_pin, (freq), (dur))
+#define STOP_TONE()
+
+#define DURATION_MS 500
+#define PAUSE_MS 500
+
+ void setup() {
+ pinMode(buzzer_pin, OUTPUT);
+}
+
+
+
+ void loop() {
+ 
+ PLAY_TONE(F, DURATION_MS);
+ PLAY_TONE(F, DURATION_MS);
+ PLAY_TONE(F, DURATION_MS);
+ PLAY_TONE(A, DURATION)
+ delay(DURATION_MS);
+
+ STOP_TONE();
+
+ delay(PAUSE_MS);
+
+}
